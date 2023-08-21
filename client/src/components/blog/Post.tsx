9e611660e7401ad22b1b280334f1a21bc9b9eddc
dony <kwakdh25@gmail.com>
2023-08-22 07:40:03 +0900

    메인페이지 아이템 디자인 수정

--- client/src/components/blog/Post.tsx
+++ client/src/components/blog/Post.tsx
@@ -4,30 +4,44 @@ import thumbnailSrc from "../../joe.jpeg";
 const ContentContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-  text-align: center;
+  text-align: left;
   justify-content: center;
   margin: 30px;
-  width: 440px;
-  height: 440px;
+  width: min-content;
+  height: min-content;
+  transition: all 0.5s linear;
+  cursor: pointer;
+  :hover {
+    transform: scale(1.1, 1.1);
+  }
 `;
 
 const ThumbnailDiv = styled.div`
+  display: flex;
   height: 400px;
   width: 400px;
+  justify-content: center;
   background-size: cover;
   background-repeat: no-repeat;
 `;
 
-const Title = styled.div`
+const ProductName = styled.div`
   width: 100%;
   padding: 5px 0px;
-  font-size: 14px;
-  font-weight: bold;
+  font-size: 16px;
+  font-family: "MusinsaMedium";
 `;
 
-const Etc = styled.div`
+const Price = styled.div`
+  width: 100%;
   font-size: 11px;
-  margin: auto;
+  font-family: "MusinsaMedium";
+`;
+
+const BrandName = styled.div`
+  width: 100%;
+  font-size: 16px;
+  font-family: "MusinsaBold";
 `;
 
 const Post = () => {
@@ -38,8 +52,9 @@ const Post = () => {
           backgroundImage: `url(https://cdn.imweb.me/thumbnail/20230425/de992e2de2480.jpg)`,
         }}
       ></ThumbnailDiv>
-      <Title>BOI hoodie</Title>
-      <Etc>43,000원</Etc>
+      <BrandName>아디다스</BrandName>
+      <ProductName>BOI hoodie</ProductName>
+      <Price>43,000원</Price>
     </ContentContainer>
   );
 };
