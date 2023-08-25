297d0eb661bb7a85d724d78f68a591dd200fb0c4
dony <kwakdh25@gmail.com>
2023-08-26 01:48:36 +0900

    리액트 라우트 홈 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import styled from "styled-components";
 import MainItemContainer from "./components/container/MainItemContainer";
 import Footer from "./components/footer/footer";
@@ -18,7 +18,10 @@ function App() {
       <GlobalStyle />
       <BrowserRouter>
         <Header />
-        <MainItemContainer />
+        <Routes>
+          <Route path="/" element={<MainItemContainer />} />
+          <Route path="/clothes" element={<div></div>} />
+        </Routes>
         <PaddingContainer />
         <PaddingContainer />
         <Footer />
