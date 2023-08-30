61a4cba5cad87c31ebd3e534f0e12ed8faded3e4
dony <kwakdh25@gmail.com>
2023-08-30 11:56:07 +0900

    옷 아이템 생성/수정 페이지 퍼블리싱

--- client/src/containers/CreateClothes/index.tsx
+++ client/src/containers/CreateClothes/index.tsx
@@ -37,6 +37,92 @@ const CreateClothes = () => {
           />
         </S.ImageInput>
       </S.Container>
+      <S.InputContainer>
+        <S.InputRowWrapper>
+          <S.InputNameWrapper>
+            <S.InputName>종류</S.InputName>
+          </S.InputNameWrapper>
+          <S.InputWrapper>
+            <select id="kind" onChange={(e) => console.log(e.target.value)}>
+              <option value="아우터">아우터</option>
+              <option value="상의">상의</option>
+              <option value="하의">하의</option>
+              <option value="신발">신발</option>
+            </select>
+          </S.InputWrapper>
+        </S.InputRowWrapper>
+        <S.InputRowWrapper>
+          <S.InputNameWrapper>
+            <S.InputName>색상</S.InputName>
+          </S.InputNameWrapper>
+          <S.InputWrapper>
+            <S.Input
+              className="colorInput"
+              type="color"
+              onChange={(e) => console.log(e.target.value)}
+            />
+          </S.InputWrapper>
+        </S.InputRowWrapper>
+        <S.InputRowWrapper>
+          <S.InputNameWrapper>
+            <S.InputName>구매일자</S.InputName>
+          </S.InputNameWrapper>
+          <S.InputWrapper>
+            <S.Input type="date" />
+          </S.InputWrapper>
+        </S.InputRowWrapper>
+        <S.InputRowWrapper>
+          <S.InputNameWrapper>
+            <S.InputName>구매한 가격</S.InputName>
+          </S.InputNameWrapper>
+          <S.InputWrapper>
+            <S.Input
+              type="text"
+              value="50,000"
+              onChange={(e) => console.log(e.target.value)}
+            />
+          </S.InputWrapper>
+        </S.InputRowWrapper>
+        <S.InputRowWrapper>
+          <S.InputNameWrapper>
+            <S.InputName>브랜드</S.InputName>
+          </S.InputNameWrapper>
+          <S.InputWrapper>
+            <S.Input
+              type="text"
+              value="노브랜드"
+              onChange={(e) => console.log(e.target.value)}
+            />
+          </S.InputWrapper>
+        </S.InputRowWrapper>
+        <S.InputRowWrapper>
+          <S.InputNameWrapper>
+            <S.InputName>구매링크</S.InputName>
+          </S.InputNameWrapper>
+          <S.InputWrapper>
+            <S.Input type="text" />
+          </S.InputWrapper>
+        </S.InputRowWrapper>
+        <S.InputRowWrapper>
+          <S.InputNameWrapper>
+            <S.InputName>사이즈</S.InputName>
+          </S.InputNameWrapper>
+          <S.InputWrapper>
+            <S.Input
+              type="text"
+              value="M"
+              onChange={(e) => console.log(e.target.value)}
+            />
+          </S.InputWrapper>
+        </S.InputRowWrapper>
+        <S.SaveBtn
+          onClick={() => {
+            console.log("Submit");
+          }}
+        >
+          저장
+        </S.SaveBtn>
+      </S.InputContainer>
     </>
   );
 };
--- client/src/containers/CreateClothes/style.ts
+++ client/src/containers/CreateClothes/style.ts
@@ -1,3 +1,4 @@
+import { ContentContainer } from "./../../components/ClothesThumbnail/style";
 import styled from "styled-components";
 
 export const Container = styled.div`
@@ -8,12 +9,15 @@ export const Container = styled.div`
 
 export const Title = styled.span`
   font-size: 24px;
+  margin-top: 30px;
 `;
 
 export const ImageInput = styled.div`
+  margin-top: 30px;
+  position: relative;
   img {
-    max-width: 325px;
-    max-height: 325px;
+    max-width: 240px;
+    max-height: 240px;
   }
   label {
     display: inline-block;
@@ -23,4 +27,79 @@ export const ImageInput = styled.div`
     width: 0;
     height: 0;
   }
+  button {
+    position: absolute;
+    background-color: lightgray;
+    color: black;
+    right: -10px;
+    top: -10px;
+    width: 25px;
+    height: 25px;
+    border: none;
+    border-left: 1px solid black;
+    border-bottom: 1px solid black;
+    border-radius: 50%;
+    text-align: center;
+    justify-content: center;
+    font-weight: bold;
+  }
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  margin-top: 30px;
+`;
+
+export const InputRowWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-right: 168px;
+  padding: 15px 0;
+  text-align: center;
+  select {
+    width: 163px;
+    text-align: center;
+    border: none;
+    border-bottom: 1px solid black;
+    :focus {
+      outline: none;
+    }
+  }
+  option {
+    background-color: black;
+  }
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 240px;
+`;
+
+export const InputNameWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 167px;
+`;
+
+export const InputName = styled.span`
+  white-space: nowrap;
+`;
+
+export const Input = styled.input`
+  border: none;
+  text-align: center;
+  border-bottom: 1px solid black;
+  :focus {
+    outline: none;
+  }
+  &.colorInput {
+    width: 25px;
+    height: 25px;
+    border: none;
+  }
+
 `;
