1331f0ead3aa4cc31a5dcaa7b2a64fac7ba9cce4
dony <kwakdh25@gmail.com>
2023-09-24 06:18:52 +0900

    내장 ValidationPipe 적용

--- server/src/users/dto/login.dto.ts
+++ server/src/users/dto/login.dto.ts
@@ -1,4 +1,9 @@
+import { IsString, MaxLength } from 'class-validator';
+
 export class LoginDto {
+  @IsString()
+  @MaxLength(20)
   readonly username: string;
+  @IsString()
   readonly password: string;
 }
