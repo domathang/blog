e94e0e2b6bac26efe4a0f2cf44d87f595b7065ac
dony <kwakdh25@gmail.com>
2023-11-14 21:49:46 +0900

    회원가입 충돌 테스트케이스 추가

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,13 +1,14 @@
 import { UsersService } from './users.service';
 import { Body, Controller, Post } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { User } from '@prisma/client';
 
 @Controller()
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post('/user')
-  async createUser(@Body() loginDto: LoginDto): Promise<void> {
+  async createUser(@Body() loginDto: LoginDto): Promise<User> {
     return this.usersService.createUser(loginDto);
   }
 }
